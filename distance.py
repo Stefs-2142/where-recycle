95831202b4270be5070e762a0d9f7404f4c599cf
zhumzhumay <aimanakaamon@gmail.com>
2020-11-22 18:49:43 +0300

    rename

--- distance.py
+++ distance.py
@@ -1,10 +1,10 @@
 
 from math import pi, asin, sin, sqrt, cos
-def dist(ame,bme, a, b):
-    amer = ame*pi/180
-    bmer = bme*pi/180
-    ar = a*pi/180
-    br = b*pi/180
+def dist(latm,longm, lat, long):
+    amer = latm*pi/180
+    bmer = longm*pi/180
+    ar = lat*pi/180
+    br = long*pi/180
     R=6371
     d= R*2*asin(sqrt(((sin((ar-amer)/2))**2)+cos(amer)*cos(ar)*((sin((br-bmer)/2))**2)))
     return d
